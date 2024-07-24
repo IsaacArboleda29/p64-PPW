aed2de923138de1abc8fc0283c3dd9b992820d2a
IsaacArboleda29 <naranjojaves1004@gmail.com>
2024-07-23 21:21:12 -0500

    act

---
+++ unidad_01/javascript/advanced/ejercicio_04/index.js
@@ -0,0 +1,8 @@
+console.log('Primera frase.')
+setTimeout(function(){
+    console.log('segunda frase')
+},2000)
+setTimeout(function(){
+    console.log('tercera frase.')
+},1000)
+console.log('Cuarta frase.')
